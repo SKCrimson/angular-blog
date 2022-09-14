f2c68480e3ff055668ffcb6e101e9f83a0a77f22
Krivošein Sergej <krivosein@syntea.cz>
2022-09-14 17:01:28 +0200

    angular blog auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyD7H-v9Ej4cDrFqAnI-YPN5YceZBOtj6nM'
 };
 
 /*
